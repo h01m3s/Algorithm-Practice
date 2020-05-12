6aabb9b7ab88d597843753b2da8f7f5c452a7049
h01m3s <lin426797@gmail.com>
2020-05-12 15:42:24 -0400

    O(n) solution

--- LeetCode/121.Best_Time_to_Buy_and_Sell_Stock.js
+++ LeetCode/121.Best_Time_to_Buy_and_Sell_Stock.js
@@ -5,19 +5,38 @@
  */
 
 // Brute force
+// const maxProfit = function (prices) {
+//   let max = 0;
+
+//   for (let i = 0; i < prices.length; i++) {
+//     for (let j = i; j < prices.length; j++) {
+//       const difference = prices[i] - prices[j];
+//       if (difference < 0 && Math.abs(difference) > max) {
+//         max = Math.abs(difference);
+//       }
+//     }
+//   }
+
+//   return max;
+// };
+
 const maxProfit = function (prices) {
+  if (prices.length <= 1) return 0;
+  let minPrice = 99999;
   let max = 0;
+  let profit = 0;
 
   for (let i = 0; i < prices.length; i++) {
-    for (let j = i; j < prices.length; j++) {
-      const profit = prices[i] - prices[j];
-      if (profit < 0 && Math.abs(profit) > max) {
-        max = Math.abs(profit);
-      }
+    const price = prices[i];
+    if (price < minPrice && i < prices.length - 1) {
+      minPrice = price;
+      max = price;
     }
+    if (price > max) max = price;
+    profit = max - minPrice > profit ? max - minPrice : profit;
   }
 
-  return max;
+  return profit;
 };
 
-console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+console.log(maxProfit([7, 6, 4, 3, 1]));
