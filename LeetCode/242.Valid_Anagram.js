3af3ae8b623f319c6457c9493ccbb27619fca4ce
h01m3s <lin426797@gmail.com>
2020-06-29 14:33:29 -0400

    Update Valid Anagram using hash table

--- LeetCode/242.Valid_Anagram.js
+++ LeetCode/242.Valid_Anagram.js
@@ -4,8 +4,26 @@
  * @param {string} t
  * @return {boolean}
  */
+// var isAnagram = function (s, t) {
+//   if (s.length !== t.length) return false;
+
+//   return s.split('').sort().join('') === t.split('').sort().join('');
+// };
+
 var isAnagram = function (s, t) {
   if (s.length !== t.length) return false;
 
-  return s.split('').sort().join('') === t.split('').sort().join('');
+  const dict = {};
+
+  for (const char of s) {
+    dict[char] = dict.hasOwnProperty(char) ? dict[char] + 1 : 1;
+  }
+
+  for (const char of t) {
+    if (!dict.hasOwnProperty(char)) return false;
+    dict[char] = dict[char] - 1;
+    if (dict[char] < 0) return false;
+  }
+
+  return true;
 };
