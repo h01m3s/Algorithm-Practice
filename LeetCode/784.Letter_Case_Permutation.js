bac3582b71d658127cbccd6bb55ca3801c9b1643
h01m3s <lin426797@gmail.com>
2020-06-30 16:46:21 -0400

    Implement Letter Case Permutation

---
+++ LeetCode/784.Letter_Case_Permutation.js
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/letter-case-permutation/
+ * @param {string} S
+ * @return {string[]}
+ */
+var letterCasePermutation = function (S) {
+  const result = [];
+
+  const recur = (text = '', i = 0) => {
+    if (text.length === S.length) {
+      result.push(text);
+      return;
+    }
+
+    if (S[i].match(/[a-z]/i)) {
+      if (S[i] === S[i].toLowerCase()) {
+        recur(text + S[i].toUpperCase(), i + 1);
+      } else {
+        recur(text + S[i].toLowerCase(), i + 1);
+      }
+    }
+    recur(text + S[i], i + 1);
+  };
+
+  recur();
+
+  return result;
+};
+
+console.log(letterCasePermutation('hey'));
