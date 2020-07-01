bcad73ad2fd56b7989ce808012eaa11dfb201eaa
h01m3s <lin426797@gmail.com>
2020-07-01 13:24:12 -0400

    Update iterative solution

--- LeetCode/784.Letter_Case_Permutation.js
+++ LeetCode/784.Letter_Case_Permutation.js
@@ -3,24 +3,47 @@
  * @param {string} S
  * @return {string[]}
  */
+
+// var letterCasePermutation = function (S) {
+//   const result = [];
+
+//   const recur = (text = '', i = 0) => {
+//     if (text.length === S.length) {
+//       result.push(text);
+//       return;
+//     }
+
+//     if (S[i].match(/[a-zA-Z]/i)) {
+//       recur(text + S[i].toUpperCase(), i + 1);
+//       recur(text + S[i].toLowerCase(), i + 1);
+//     } else {
+//       recur(text + S[i], i + 1);
+//     }
+//   };
+
+//   recur();
+
+//   return result;
+// };
+
+// iterative
 var letterCasePermutation = function (S) {
-  const result = [];
+  let result = [''];
 
-  const recur = (text = '', i = 0) => {
-    if (text.length === S.length) {
-      result.push(text);
-      return;
-    }
-
-    if (S[i].match(/[a-z]/i)) {
-      recur(text + S[i].toUpperCase(), i + 1);
-      recur(text + S[i].toLowerCase(), i + 1);
+  for (let current of S) {
+    if (current.match(/[a-zA-Z]/i)) {
+      const arr = [];
+      for (let s of result) {
+        arr.push(s + current.toUpperCase());
+        arr.push(s + current.toLowerCase());
+      }
+      result = arr;
     } else {
-      recur(text + S[i], i + 1);
+      for (let j = 0; j < result.length; j++) {
+        result[j] = result[j] + current;
+      }
     }
-  };
-
-  recur();
+  }
 
   return result;
 };
