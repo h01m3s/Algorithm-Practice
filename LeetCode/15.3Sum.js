21df8e71cb4c00d50cf7c14ff4dbd8ebf621a15a
h01m3s <lin426797@gmail.com>
2020-07-08 23:36:06 -0400

    Implement arrangeCoins

---
+++ LeetCode/15.3Sum.js
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode.com/problems/3sum/
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+var threeSum = function (nums) {
+  for (let target of nums) {
+    for (let num of nums) {
+    }
+  }
+};
