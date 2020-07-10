858bd5647ea42e95f08206af4bc3afdeaecb4812
h01m3s <lin426797@gmail.com>
2020-07-10 14:05:28 -0400

    Implement 3Sum

--- LeetCode/15.3Sum.js
+++ LeetCode/15.3Sum.js
@@ -5,8 +5,48 @@
  */
 
 var threeSum = function (nums) {
-  for (let target of nums) {
-    for (let num of nums) {
+  const result = [];
+
+  nums = nums.sort((a, b) => a - b);
+
+  let previous = undefined;
+
+  for (let i = 0; i < nums.length; i++) {
+    const a = nums[i];
+    if (a === previous) {
+      continue;
+    } else {
+      previous = a;
+    }
+    let left = i + 1;
+    let right = nums.length - 1;
+    let b = nums[left];
+    let c = nums[right];
+
+    while (left < right) {
+      const sum = a + b + c;
+
+      if (sum === 0) {
+        result.push([a, b, c]);
+        left++;
+        while (nums[left] === b) {
+          left++;
+        }
+        b = nums[left];
+        right--;
+        while (nums[right] === c) {
+          right--;
+        }
+        c = nums[right];
+      } else if (sum > 0) {
+        right--;
+        c = nums[right];
+      } else {
+        left++;
+        b = nums[left];
+      }
     }
   }
+
+  return result;
 };
