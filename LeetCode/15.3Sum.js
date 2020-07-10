8b67b468635d5fa104477e9ed82897d260d252dd
h01m3s <lin426797@gmail.com>
2020-07-10 14:24:34 -0400

    Add comment and code simplified

--- LeetCode/15.3Sum.js
+++ LeetCode/15.3Sum.js
@@ -7,43 +7,35 @@
 var threeSum = function (nums) {
   const result = [];
 
-  nums = nums.sort((a, b) => a - b);
+  nums.sort((a, b) => a - b);
 
-  let previous = undefined;
+  // dont have to check last tow nums
+  for (let i = 0; i < nums.length - 2; i++) {
+    // if nums[i] > 0, no solution
+    if (nums[i] > 0) break;
 
-  for (let i = 0; i < nums.length; i++) {
-    const a = nums[i];
-    if (a === previous) {
-      continue;
-    } else {
-      previous = a;
-    }
+    // resolve duplicate nums[i]
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
     let left = i + 1;
     let right = nums.length - 1;
-    let b = nums[left];
-    let c = nums[right];
 
     while (left < right) {
-      const sum = a + b + c;
+      const sum = nums[i] + nums[left] + nums[right];
 
       if (sum === 0) {
-        result.push([a, b, c]);
+        result.push([nums[i], nums[left], nums[right]]);
+
+        // loop until last same number
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right + 1]) right--;
         left++;
-        while (nums[left] === b) {
-          left++;
-        }
-        b = nums[left];
         right--;
-        while (nums[right] === c) {
-          right--;
-        }
-        c = nums[right];
       } else if (sum > 0) {
         right--;
-        c = nums[right];
       } else {
+        // sum < 0
         left++;
-        b = nums[left];
       }
     }
   }
