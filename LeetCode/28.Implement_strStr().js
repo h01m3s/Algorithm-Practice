80b38fca356eccb34ef8e7dd0bd15d5ed0fd7e18
h01m3s <lin426797@gmail.com>
2020-08-10 15:54:11 -0400

    Implement strStr()

---
+++ LeetCode/28.Implement_strStr().js
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/implement-strstr/
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  if (!needle) return 0;
+
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    for (let j = 0; j <= needle.length - 1; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        break;
+      }
+      if (j === needle.length - 1) {
+        return i;
+      }
+    }
+  }
+
+  return -1;
+};
