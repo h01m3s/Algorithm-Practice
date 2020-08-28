42767835a8bca4b988a7cd3718d0e61db284ff88
h01m3s <lin426797@gmail.com>
2020-08-28 15:16:26 -0400

    Implement with Expand Around Center

--- LeetCode/5.Longest_Palindromic_Substring.js
+++ LeetCode/5.Longest_Palindromic_Substring.js
@@ -26,20 +26,50 @@ const isSubPalindrome = (str, l, r) => {
 
 // Time Complexity: O(n^3)
 // Space Complexity: O(1)
+// var longestPalindrome = function (s) {
+//   const len = s.length;
+//   if (len < 2) return s;
+
+//   let max = 1;
+//   let start = 0;
+
+//   for (let i = 0; i < len - 1; i++) {
+//     for (let j = i + 1; j < len; j++) {
+//       if (j - i + 1 > max && isSubPalindrome(s, i, j)) {
+//         max = j - i + 1;
+//         start = i;
+//       }
+//     }
+//   }
+
+//   return s.slice(start, start + max);
+// };
+
+// Expand Around Center
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
 var longestPalindrome = function (s) {
   const len = s.length;
   if (len < 2) return s;
+
+  const extend = (l, r) => {
+    while (l >= 0 && r < len && s[l] === s[r]) {
+      l--;
+      r++;
+    }
+
+    if (r - l - 1 > max) {
+      start = l + 1;
+      max = r - l - 1;
+    }
+  };
 
   let max = 1;
   let start = 0;
 
   for (let i = 0; i < len - 1; i++) {
-    for (let j = i + 1; j < len; j++) {
-      if (j - i + 1 > max && isSubPalindrome(s, i, j)) {
-        max = j - i + 1;
-        start = i;
-      }
-    }
+    extend(i, i);
+    extend(i, i + 1);
   }
 
   return s.slice(start, start + max);
