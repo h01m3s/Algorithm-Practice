71e2f86b01ceb819649a0677ddce0388c21060a0
h01m3s <lin426797@gmail.com>
2020-08-31 14:05:01 -0400

    Use Math

--- LeetCode/7.Reverse_Integer.js
+++ LeetCode/7.Reverse_Integer.js
@@ -2,11 +2,17 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function (x) {
-  const limit = 2147483648;
-  const k = x < 0 ? -1 : 1;
-  const n = Number(String(Math.abs(x)).split('').reverse().join(''));
-  return n > limit ? 0 : n * k;
-};
+// var reverse = function (x) {
+//   const k = x < 0 ? -1 : 1;
+//   const n = Number(String(Math.abs(x)).split('').reverse().join(''));
+//   return n > Math.pow(2, 31) ? 0 : n * k;
+// };
 
-console.log(reverse(123));
+var reverse = function (x) {
+  let ans = 0;
+  while (x !== 0) {
+    ans = ans * 10 + (x % 10);
+    x = Math.trunc(x / 10);
+  }
+  return Math.abs(ans) > Math.pow(2, 31) ? 0 : ans;
+};
